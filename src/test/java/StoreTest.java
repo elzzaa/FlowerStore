2fb2eafef834bde5dbcba220778cb3c6e02fa917
elzzaa <oksana.sadova@ucu.edu.ua>
2021-10-25 23:12:02 +0300

    commit 2

---
+++ src/test/java/StoreTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreTest {
+    private Flower flower;
+    private Store store;
+    @BeforeEach
+    void setUp() {
+        flower = new Flower(FlowerType.ROSE);
+        store = new Store();
+        store.assortment = List.of(new FlowerPack(flower));
+    }
+
+    @Test
+    void search() { assertFalse(store.search(flower)); }
+}
